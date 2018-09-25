9db262e71fd34acb8629fdc5cef82e29cec5401a
mukulvtilak <tilak.mukul@hotmail.com>
2018-09-25 11:15:26 +0530

    route for student details

--- src/services/_routerService.js
+++ src/services/_routerService.js
@@ -1,10 +1,15 @@
 
 import Dashboard from 'Routes/dashboard';
+import StudentDetails from 'Routes/studentDetails'
 
 
 export default [
 	{
 		path: 'dashboard',
 		component: Dashboard
+	},
+	{
+		path: '',
+		component: StudentDetails
 	}
 ]
